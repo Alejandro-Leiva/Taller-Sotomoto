fbd058e9a81ada73125ac0519a32aaf13152b988
Alejandro Leiva <alex-lp@hotmail.es>
2020-12-15 11:14:24 +0100

    Actualización aplicación

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCFFlfb-kMf0rDLCSg_yFT8o4RZ7sQKn0o",
+    authDomain: "tallersotomoto.firebaseapp.com",
+    databaseURL: "https://tallersotomoto.firebaseio.com",
+    projectId: "tallersotomoto",
+    storageBucket: "tallersotomoto.appspot.com",
+    messagingSenderId: "23135494322",
+    appId: "1:23135494322:web:eca81dad2f2eeebd7969ab"
+  }
 };
